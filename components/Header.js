ae13d1ab346ae40bf67b662cfb8c82e4b3534411
Aditya Rajvanshi <aditya.rajvanshi18@gmail.com>
2021-09-11 15:11:05 -0700

    small fix to mobile

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { GlobeAltIcon, MenuIcon, UserCircleIcon, UsersIcon, SearchIcon } from '@heroicons/react/solid'
+import { GlobeAltIcon, MenuIcon, UserCircleIcon, UsersIcon, SearchIcon, ChevronLeftIcon } from '@heroicons/react/solid'
 import { useState } from "react";
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
@@ -44,16 +44,18 @@ function Header({ placeholder }) {
     return (
         <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
             {/* left */}
-            <div onClick={()=> router.push("/")} className='relative flex items-center h-10 cursor-pointer my-auto'>
+            <div onClick={()=> router.push("/")} className='flex relative items-center h-10 cursor-pointer my-auto'>
                 <Image
                     src='https://links.papareact.com/qd3'
                     layout='fill'
                     objectFit='contain'
                     objectPosition='left'
                 />
+                
             </div>
+            
             {/* middle */}
-            <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
+            <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm '>
                 <input 
                     value={searchInput}
                     onChange={(e)=> setSearchInput(e.target.value)}
@@ -67,7 +69,7 @@ function Header({ placeholder }) {
             </div>
 
             {/* right */}
-            <div className="flex items-center space-x-4 justify-end text-gray-500 ">
+            <div className="hidden sm:inline-flex items-center space-x-4 justify-end text-gray-500 ">
                 <p className="hidden md:inline cursor-pointer">Become a host</p>
                 <GlobeAltIcon className="hidden md:inline h-6 cursor-pointer"/>
                 <div className="flex items-center space-x-2 border-2 p-2 rounded-full cursor-pointer">
