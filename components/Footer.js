75a4236508d574e1c338a61315e9c83f93ece81d
Aditya Rajvanshi <aditya.rajvanshi18@gmail.com>
2021-09-10 21:05:07 -0700

    day 1 build main landing done

---
+++ components/Footer.js
@@ -0,0 +1,44 @@
+function Footer() {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-4 px-32 py-14 bg-gray-100 text-gray-600">
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">About</h5>
+                <p>How Airbnb works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>Airbnb Plus</p>
+                <p>Airbnb Luxe</p>
+            </div>
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">COMMUNITY</h5>
+                <p>Diversify & Belonging</p>
+                <p>Accessibility</p>
+                <p>This is not a real website</p>
+                <p>Gift Cards</p>
+                <p>Airbnb.org</p>
+            </div>
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">HOST</h5>
+                <p>Host your home</p>
+                <p>A really good clone website</p>
+                <p>Host an Online Experience</p>
+                <p>Host an Experience</p>
+                <p>Responsible hosting</p>
+            </div >
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">SUPPORT</h5>
+                <p>Our COVID-19 Response</p>
+                <p>Help Center</p>
+                <p>Cancellation options</p>
+                <p>Neighbourhood Support</p>
+                <p>Trust & Safety</p>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Footer
